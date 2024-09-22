6a77870784537954388e0ec1bdffc46ec18f2506
Imash Dilharsha <imashbigdil@gmail.com>
2024-09-22 18:49:13 +0530

    FronEnd - Done

---
+++ app/src/main/java/com/wlghost/sas/ImageSliderAdapter.java
@@ -0,0 +1,50 @@
+package com.wlghost.sas;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.ArrayList;
+
+public class ImageSliderAdapter extends PagerAdapter {
+
+    private Context context;
+    private ArrayList<Integer> images;
+    private LayoutInflater inflater;
+
+    public ImageSliderAdapter(Context context, ArrayList<Integer> images) {
+        this.context = context;
+        this.images = images;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = inflater.inflate(R.layout.image_slider_layout, container, false);
+        ImageView imageView = view.findViewById(R.id.sliderImage);
+        imageView.setImageResource(images.get(position));
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
