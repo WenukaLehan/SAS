3a82d70dd1df3f31f488a834e8cd3b38ab467de1
wenuka <wmwenuka@gmail.com>
2024-11-16 11:09:15 +0530

    login backend5

--- app/src/main/java/com/wlghost/sas/activity_announcements.java
+++ app/src/main/java/com/wlghost/sas/activity_announcements.java
@@ -41,7 +41,7 @@ public class activity_announcements extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_announcements);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main8), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
