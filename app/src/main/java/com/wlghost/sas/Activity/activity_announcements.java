3ddebf6b7de77e44c4b08b766e9e456e24730845
Kavishka <kavishkasupunpathiraja@gmail.com>
2024-11-23 14:19:36 +0530

    nav_bar2 done

--- app/src/main/java/com/wlghost/sas/Activity/activity_announcements.java
+++ app/src/main/java/com/wlghost/sas/Activity/activity_announcements.java
@@ -1,21 +1,30 @@
 package com.wlghost.sas.Activity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.DocumentReference;
 import com.wlghost.sas.Helper.SessionManager;
 import com.wlghost.sas.Helper.dbCon;
@@ -36,12 +45,58 @@ public class activity_announcements extends AppCompatActivity {
     private static String sender;
     private static String receiver;
 
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_announcements);
+
+
+        drawerLayout = findViewById(R.id.main8);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_teacherannounce);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        // Handle Navigation Item Clicks
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    Intent intent = new Intent(activity_announcements.this, activity_teacher_dashboard.class);
+                    startActivity(intent);
+                }  else if (id == R.id.nav_logout) {
+                    if(sessionManager.isLoggedIn()){
+                        sessionManager.logoutUser();
+                        startActivity(new Intent(activity_announcements.this, login_activity.class));
+                        finish();
+                        Toast.makeText(activity_announcements.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(activity_announcements.this, "Unknown item clicked", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START); // Close the drawer
+                return true;
+            }
+        });
+
+        // Set the navigation view initially hidden
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main8), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/wlghost/sas/Activity/activity_mysubjects.java
+++ app/src/main/java/com/wlghost/sas/Activity/activity_mysubjects.java
@@ -3,20 +3,26 @@ package com.wlghost.sas.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -49,6 +55,10 @@ public class activity_mysubjects extends AppCompatActivity implements OnISubject
     private DocumentReference db;
     private SessionManager sessionManager;
 
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
     private static final String TAG = "activity_mysubjects";
     private String teacherId;
     @Override
@@ -56,6 +66,47 @@ public class activity_mysubjects extends AppCompatActivity implements OnISubject
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mysubjects);
+
+
+        drawerLayout = findViewById(R.id.main9);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_teachersubject);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        // Handle Navigation Item Clicks
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    Intent intent = new Intent(activity_mysubjects.this, activity_teacher_dashboard.class);
+                    startActivity(intent);
+                }  else if (id == R.id.nav_logout) {
+                    if(sessionManager.isLoggedIn()){
+                        sessionManager.logoutUser();
+                        startActivity(new Intent(activity_mysubjects.this, login_activity.class));
+                        finish();
+                        Toast.makeText(activity_mysubjects.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(activity_mysubjects.this, "Unknown item clicked", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START); // Close the drawer
+                return true;
+            }
+        });
+
+        // Set the navigation view initially hidden
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main9), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
